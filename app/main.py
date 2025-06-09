445bf15a9492e36f153745c140564c27d4116542
Ryan A Clark <ryan.a.clark.fullstack.dev@gmail.com>
2025-06-08 22:34:43 -0500

    added static html page to root

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.api.main import api_router
 
 app = FastAPI(
@@ -6,5 +7,7 @@ app = FastAPI(
     description="This is a sample FastAPI application.",
     version="1.0.0",
 )
+
+app.mount("/", StaticFiles(directory="app/static", html = True), name="static")
 
 app.include_router(api_router)
