16a09fe140382b801d8fec2b6e3641df11db484f
Ryan A Clark <ryan.a.clark.fullstack.dev@gmail.com>
2025-06-08 22:04:06 -0500

    changed health NN to dynamic to make a frontend capable of showing how NN works and training

--- app/neural_network/dynamic_simple_nn.py
+++ app/neural_network/dynamic_simple_nn.py
@@ -1,24 +1,5 @@
 import numpy as np
 
-average_male = {
-    'height': 69, # inches
-    'weight': 198, # pounds
-    'age': 50, # years
-    'bmi': 29.5, # kg/m^2
-}
-
-average_female = {
-    'height': 63, # inches
-    'weight': 170, # pounds
-    'age': 50, # years
-    'bmi': 29.5, # kg/m^2
-}
-
-average_height = average_male['height'] + average_female['height'] / 2
-average_weight = average_male['weight'] + average_female['weight'] / 2
-average_age = average_male['age'] + average_female['age'] / 2
-average_bmi = average_male['bmi'] + average_female['bmi'] / 2
-
 def sigmoid(x):
   # Sigmoid activation function: f(x) = 1 / (1 + e^(-x))
   return 1 / (1 + np.exp(-x))
@@ -32,7 +13,7 @@ def mse_loss(y_true, y_pred):
   # y_true and y_pred are numpy arrays of the same length.
   return ((y_true - y_pred) ** 2).mean()
 
-class HealthNN:
+class Dynamic_Simple_NN:
     """A simple neural network for health predictions."""
     
     def __init__(self, inputs=2, outputs=1, layers=1):
@@ -61,20 +42,3 @@ class HealthNN:
         output += self.biases[-1]
         
         return sigmoid(output)
-
-x = HealthNN()
-print(f"Inputs: {x.inputs}, Outputs: {x.outputs}, Biases: {x.biases}, Weights: {x.weights}") 
-# Define dataset
-data = np.array([
-  [-2, -1],  # Alice
-  [25, 6],   # Bob
-  [17, 4],   # Charlie
-  [-15, -6], # Diana
-])
-all_y_trues = np.array([
-  1, # Alice
-  0, # Bob
-  0, # Charlie
-  1, # Diana
-])
-print(f"Forward pass output for Alice: {x.forward([-2, -1])}")  # Should print the output for Alice
