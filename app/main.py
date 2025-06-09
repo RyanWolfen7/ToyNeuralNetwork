e049c1b75bf789318fdf0e7fc921cf084a2ce37e
Ryan A Clark <ryan.a.clark.fullstack.dev@gmail.com>
2025-06-08 23:05:13 -0500

    added cors to the main

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.main import api_router
 
 app = FastAPI(
     title="ToyNeuralNetwork",
     description="This is a sample FastAPI application.",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["127.0.0.1:54147", "127.0.0.1:3000"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.mount("/", StaticFiles(directory="app/static", html = True), name="static")
